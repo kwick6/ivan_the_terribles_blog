34cf773ac39eec5b3e9855cf9dbe65d614e4d2f0
SG <sy1840@gmail.com>
2013-11-21 10:46:19 -0800

    comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,8 @@ class CommentsController < ApplicationController
   # GET /comments.json
   def index
     @comments = Comment.all
+
+    respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @comments }
     end
