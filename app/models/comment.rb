9e30d7b54be714650371aa405d6a7048b6e2e5b7
SG <sy1840@gmail.com>
2013-11-21 09:52:49 -0800

    move comments to post show page

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,6 @@ class Comment < ActiveRecord::Base
   attr_accessible :body, :post
   belongs_to :post
   has_many :replies, dependent: :destroy
+  self.per_page = 10
+  scope :recent, limit(10).order("created_at ASC")
 end
